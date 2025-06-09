2a59e76a1c2f1758a40233a06eb7a156391d62ab
Justin Ta <justintaa1536@gmail.com>
2025-06-08 23:18:20 -0500

    Add UndoablePenDownCommand to support reversible pen lowering

--- src/main/java/command/UndoablePenDownCommand.java
+++ src/main/java/command/UndoablePenDownCommand.java
@@ -1,4 +1,21 @@
 package command;
 
-public class UndoablePenDownCommand {
+import turtle.Turtle;
+
+public class UndoablePenDownCommand implements UndoableCommand {
+    private final Turtle turtle;
+
+    public UndoablePenDownCommand(Turtle turtle) {
+        this.turtle = turtle;
+    }
+
+    @Override
+    public void execute() {
+        turtle.penDown();
+    }
+
+    @Override
+    public void undo() {
+        turtle.penUp();
+    }
 }
