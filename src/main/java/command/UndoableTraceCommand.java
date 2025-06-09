c1ef8100ecee396347bb022735d336973c2efaa5
Justin Ta <justintaa1536@gmail.com>
2025-06-08 21:16:23 -0500

    Fix undo logic to prevent redrawing during undo for move and trace

--- src/main/java/command/UndoableTraceCommand.java
+++ src/main/java/command/UndoableTraceCommand.java
@@ -1,9 +1,3 @@
-//------------------------------------------------------
-// Assignment 4
-// Written by: Justin Ta, 2148824
-// For SOEN350 Section 610 – Spring 2025
-//------------------------------------------------------
-
 package command;
 
 import turtle.Turtle;
