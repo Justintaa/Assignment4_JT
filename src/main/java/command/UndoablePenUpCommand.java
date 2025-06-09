7c352ac9d2c7664f5b3d9e5d14b0c7688af02529
Justin Ta <justintaa1536@gmail.com>
2025-06-08 23:18:06 -0500

    Add UndoablePenUpCommand to support reversible pen lifting

--- src/main/java/command/UndoablePenUpCommand.java
+++ src/main/java/command/UndoablePenUpCommand.java
@@ -1,4 +1,21 @@
 package command;
 
-public class UndoablePenUpCommand {
+import turtle.Turtle;
+
+public class UndoablePenUpCommand implements UndoableCommand {
+    private final Turtle turtle;
+
+    public UndoablePenUpCommand(Turtle turtle) {
+        this.turtle = turtle;
+    }
+
+    @Override
+    public void execute() {
+        turtle.penUp();
+    }
+
+    @Override
+    public void undo() {
+        turtle.penDown();
+    }
 }
